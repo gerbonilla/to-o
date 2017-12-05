fb42c97c61dcf00182792346d9fd0cfe16ff11d5
Gerardo Bonilla <gerbonilla@gmail.com>
2017-12-05 22:03:09 +0100

    trying to fix auto zoom on mobile

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -1,14 +1,34 @@
 class AutosController < ApplicationController
   skip_before_action :authenticate_user!
 
+  def show
+    @auto = Auto.new()
+  end
+
   def new
     @auto = Auto.new()
   end
 
   def create
+    @auto = Auto.new(auto_params)
+    if @auto.save!
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def show
-    @auto = Auto.new()
+  private
+
+  def auto_params
+    params.require(:auto).permit(:firstName,
+                                 :lastName,
+                                 :email,
+                                 :autoBrand,
+                                 :autoModel,
+                                 :ownerDUI,
+                                 :ownerDOB,
+                                 :ownerPhone)
   end
+
 end
